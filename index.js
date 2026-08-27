5c1ccab187e3719844f124589a4ab25f51772bce
Avanti Pandit <avanti.pandit5@gmail.com>
2026-08-27 13:50:52 -0400

    Add CORS support for frontend integration

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const { Pool } = require('pg');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
